75f65ebc7e0b4e1a76ea9f8f03418448165a2abd
Iurii Bliuchak <zero.ukr@gmail.com>
2018-09-22 22:53:57 +0200

    GH-3: Add unit tests for DeleteHero handler

--- internal/server/handlers/hero_test.go
+++ internal/server/handlers/hero_test.go
@@ -186,3 +186,90 @@ func TestHeroHandler_GetHeroHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestHeroHandler_DeleteHeroHandler(t *testing.T) {
+	tests := []struct {
+		name     string
+		storage  []TestifyMockCall
+		expected expected
+	}{
+		{
+			name: "should return HeroNotExists error",
+			storage: []TestifyMockCall{
+				{
+					Method: "DeleteHero",
+					Call: []interface{}{
+						AnythingOfType("string"),
+					},
+					Response: []interface{}{
+						storage.NewErrNothingToDelete("dummy"),
+					},
+				},
+			},
+			expected: expected{
+				code: http.StatusNotFound,
+			},
+		},
+		{
+			name: "should return internal server error",
+			storage: []TestifyMockCall{
+				{
+					Method: "DeleteHero",
+					Call: []interface{}{
+						AnythingOfType("string"),
+					},
+					Response: []interface{}{
+						errors.New("dummy"),
+					},
+				},
+			},
+			expected: expected{
+				code: http.StatusInternalServerError,
+			},
+		},
+		{
+			name: "should successfully delete hero",
+			storage: []TestifyMockCall{
+				{
+					Method: "DeleteHero",
+					Call: []interface{}{
+						AnythingOfType("string"),
+					},
+					Response: []interface{}{
+						nil,
+					},
+				},
+			},
+			expected: expected{
+				code: http.StatusNoContent,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodDelete, "/hero/1", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+
+			s := new(stmocks.Storager)
+			for _, mockCall := range tt.storage {
+				s.On(mockCall.Method, mockCall.Call...).Return(mockCall.Response...)
+			}
+
+			hh := HeroHandler{}
+			hh.SetStorage(s)
+
+			handler := http.HandlerFunc(hh.DeleteHeroHandler)
+
+			handler.ServeHTTP(rr, req)
+
+			if rr.Code != tt.expected.code {
+				t.Errorf("handler returned unexpected response code: got %v want %v",
+					rr.Code, tt.expected.code)
+			}
+		})
+	}
+}
