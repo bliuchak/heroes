524f8cae1153bcc2e16bab4a65d80b6c602a17a1
Iurii Bliuchak <zero.ukr@gmail.com>
2018-09-29 21:00:30 +0200

    GH-3: add unit tests for GetHeroes and CreateHero handlers

--- internal/server/handlers/common.go
+++ internal/server/handlers/common.go
@@ -1,16 +1,18 @@
 package handlers
 
 import (
-	"github.com/bliuchak/heroes/internal/server/json"
+	"encoding/json"
+
 	"github.com/bliuchak/heroes/internal/storage"
 	"github.com/rs/zerolog"
 )
 
 // CommonHandler strucute for basic dependencies for rest handlers
 type CommonHandler struct {
-	Storage storage.Storager
-	Logger  zerolog.Logger
-	JSON    json.Marshaler
+	Storage     storage.Storager
+	Logger      zerolog.Logger
+	Marshaler   func(v interface{}) ([]byte, error)
+	Unmarshaler func(data []byte, v interface{}) error
 }
 
 // SetLogger sets logger
@@ -23,7 +25,20 @@ func (ch *CommonHandler) SetStorage(st storage.Storager) {
 	ch.Storage = st
 }
 
-// SetJSON sets JSON dependency for encoding and decoding JSON
-func (ch *CommonHandler) SetJSON(j json.Marshaler) {
-	ch.JSON = j
+// Marshal will marshal the provided value with the Marshaller defined on ch.
+// If un-set, json.Marshal will be used.
+func (ch *CommonHandler) Marshal(v interface{}) ([]byte, error) {
+	if ch.Marshaler == nil {
+		return json.Marshal(v)
+	}
+	return ch.Marshaler(v)
+}
+
+// Unmarshal will unmarshal the provided value with the Unmarshaler defined on ch.
+// If un-set, json.Unmarshal will be used.
+func (ch *CommonHandler) Unmarshal(data []byte, v interface{}) error {
+	if ch.Unmarshaler == nil {
+		return json.Unmarshal(data, &v)
+	}
+	return ch.Unmarshaler(data, &v)
 }
