b10d828b215ac5b0126f4c04e8adf74521e63708
Iurii Bliuchak <zero.ukr@gmail.com>
2018-12-19 18:38:36 +0100

    Add graceful shutdown of http server

--- cmd/heroes/main.go
+++ cmd/heroes/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/bliuchak/heroes/internal"
 	"github.com/bliuchak/heroes/internal/config"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -24,6 +29,21 @@ func main() {
 	if err != nil {
 		app.Logger.Error().Err(err).Msg("Unable to init storage")
 	}
+
+	// create channel to collect terminate or interrupt signals
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		sig := <-gracefulStop
+		timeout := 5 * time.Second
+		app.Logger.Info().Interface("sig", sig).Dur("timeout", timeout).Msg("Graceful shutdown with timeout")
+		err := app.Server.Stop(timeout)
+		if err != nil {
+			app.Logger.Error().Err(err).Msg("Graceful shutdown error")
+		}
+		app.Logger.Info().Interface("sig", sig).Msg("Server stopped")
+	}()
 
 	err = app.Run()
 	if err != nil {
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 	"time"
@@ -17,6 +18,7 @@ type Serverer interface {
 	InitRouter()
 	SetMiddleware()
 	Run() error
+	Stop(timeout time.Duration) error
 }
 
 // Server app container for main dependencies
@@ -25,6 +27,8 @@ type Server struct {
 	Storage storage.Storager
 	Logger  zerolog.Logger
 	Config  config.Config
+
+	httpServer *http.Server
 }
 
 // NewServer create pointer for new server structure
@@ -36,7 +40,7 @@ func NewServer(storage storage.Storager, logger zerolog.Logger, config config.Co
 	}
 }
 
-// InitRouter initnialise router
+// InitRouter initialize router
 func (s *Server) InitRouter() {
 	s.Router = mux.NewRouter()
 }
@@ -56,16 +60,23 @@ func (s *Server) Run() error {
 
 	s.SetMiddleware()
 
-	server := &http.Server{
+	s.httpServer = &http.Server{
 		Handler:      s.Router,
 		Addr:         ":" + strconv.Itoa(s.Config.Server.Port),
 		WriteTimeout: 1 * time.Second,
 		ReadTimeout:  1 * time.Second,
 	}
 
-	err := server.ListenAndServe()
+	err := s.httpServer.ListenAndServe()
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// Stop shutdown http server with timeout
+func (s *Server) Stop(timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	return s.httpServer.Shutdown(ctx)
 }
